8db1c284c33ecd59cc2be9665c164a54e414ec55
ste87 <ste87@client1.webrtcvw12.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be>
2017-03-30 05:46:27 -0600

    modified configuration script

--- client_config.sh
+++ client_config.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo dpkg -i google-chrome*.deb; sudo apt-get -f install -y && sudo dpkg -i google-chrome*.deb
 sudo apt-get install -y xvfb libnss3-tools
 
-xvfb-run google-chrome &> /dev/null &
+xvfb-run -a google-chrome &> /dev/null &
 sudo kill $(ps aux | grep xvfb | awk '{print $2}') &> /dev/null
 sudo kill $(ps aux | grep chrome | awk '{print $2}') &> /dev/null
 
