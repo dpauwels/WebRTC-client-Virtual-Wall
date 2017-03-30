2efcea1541e1caa666014122d5a34d0f76ba97e3
dripauwe <Dries.Pauwels@UGent.be>
2017-03-30 13:37:25 +0200

    Update client_config.sh

--- client_config.sh
+++ client_config.sh
@@ -3,5 +3,9 @@ sudo apt-get update
 sudo dpkg -i google-chrome*.deb; sudo apt-get -f install -y && sudo dpkg -i google-chrome*.deb
 sudo apt-get install -y xvfb libnss3-tools
 
+xvfb-run google-chrome &> /dev/null &
+sudo kill $(ps aux | grep xvfb | awk '{print $2}') &> /dev/null
+sudo kill $(ps aux | grep chrome | awk '{print $2}') &> /dev/null
+
 certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "webrtc" -i cert.crt
 
