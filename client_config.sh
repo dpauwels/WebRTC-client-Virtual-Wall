20b64e88530bbe71bb799cc2d07cc7fc650f2a5c
dripauwe <Dries.Pauwels@UGent.be>
2017-03-31 15:21:08 +0200

    Update client_config.sh

--- client_config.sh
+++ client_config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd WebRTC-client-Virtual-Wall/
 sudo apt-get update
 sudo dpkg -i google-chrome*.deb; sudo apt-get -f install -y && sudo dpkg -i google-chrome*.deb
 sudo apt-get install -y xvfb libnss3-tools
